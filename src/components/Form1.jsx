e28f87c424b52590f6ded11e092d01d0b7b79a73
FacuMorel <facumorel1996@gmail.com>
2022-09-05 20:15:13 -0300

    localstorage

---
+++ src/components/Form1.jsx
@@ -0,0 +1,19 @@
+import { useLocalStorage } from "./useLocalStorage";
+const Form = () => {
+  const [name, setName] = useLocalStorage("name", "");
+
+  return (
+    <form>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Full name"
+        aria-label="fullname"
+      />
+      <input type="submit" value="Submit"></input>
+    </form>
+  );
+};
+
+export default Form;
